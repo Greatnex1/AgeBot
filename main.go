33ff456bfca3990445cba2861d2c464273e537ab
Nouah <noahgreatakoni@gmail.com>
2023-07-21 05:00:38 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/shomali11/slacker"
+	"log"
+	"os"
+	"strconv"
+)
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+
+	}
+}
+
+func main() {
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("My year of birth is <year>", &slacker.CommandDefinition{
+		Description: "year of birth calculator",
+		///Examples:    "my year of birth   is  2020",
+		Handler: func(botContext slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err != nil {
+				log.Fatal("error")
+			}
+			age := 2023 - yob
+			r := fmt.Sprintf("age is %d", age)
+			fmt.Println(r)
+			response.Reply(r)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
