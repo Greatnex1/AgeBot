c37657970989f5d2ba3a9aebef06f14b4c604696
Nouah <noahgreatakoni@gmail.com>
2023-08-01 15:34:12 +0100

    clean code

--- main.go
+++ main.go
@@ -1,53 +1,12 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"github.com/shomali11/slacker"
-	"log"
-	"os"
-	"strconv"
+	"AgeBot/bot"
+	"github.com/joho/godotenv"
 )
 
-func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
-	for event := range analyticsChannel {
-		fmt.Println("Command Events")
-		fmt.Println(event.Timestamp)
-		fmt.Println(event.Command)
-		fmt.Println(event.Parameters)
-		fmt.Println(event.Event)
-		fmt.Println()
-
-	}
-}
-
 func main() {
+	godotenv.Load(".env")
 
-	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
-
-	go printCommandEvents(bot.CommandEvents())
-
-	bot.Command("My year of birth is <year>", &slacker.CommandDefinition{
-		Description: "year of birth calculator",
-		///Examples:    "my year of birth   is  2020",
-		Handler: func(botContext slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
-			year := request.Param("year")
-			yob, err := strconv.Atoi(year)
-			if err != nil {
-				log.Fatal("error")
-			}
-			age := 2023 - yob
-			r := fmt.Sprintf("age is %d", age)
-			fmt.Println(r)
-			response.Reply(r)
-		},
-	})
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	err := bot.Listen(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
+	bot.AgeLoad()
 }
